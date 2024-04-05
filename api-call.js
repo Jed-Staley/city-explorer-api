c64cc3365ca795db902868e1308cb44b782cbe1c
Jed Staley <JNStaley1@gmail.com>
2024-04-05 18:44:17 -0400

    Added watchers for debugging

--- api-call.js
+++ api-call.js
@@ -4,13 +4,17 @@ const cache = require('./cache');
 const callAPI = (app, receiveURL, cacheProp, requestURL, dataMassage) => {
   app.get(receiveURL, async (req, res) => {
     const city = req.query.city;
-    if (cache[city]) {
+    console.log('city:', city);
+    if (cache[city] !== undefined) {
+      console.log('Acessing cache');
       const currentTime = new Date();
       const expired = (currentTime.getTime() - cache[city].timeCached.getTime()) > 86400000;
       const dayPassed = currentTime.getDate() !== cache[city].timeCached.getDate();
       if (!expired && !dayPassed) {
+        console.log('Reading cache');
         return res.json(cache[city][cacheProp]);
       }
+      console.log('cache outdated');
     }
   
     try {
@@ -18,6 +22,7 @@ const callAPI = (app, receiveURL, cacheProp, requestURL, dataMassage) => {
       const response = await fetch(requestURL(city));
       const parsedData = await response.json();
       const massagedData = dataMassage(parsedData);
+      console.log('massaged:', massagedData);
 
       if (!massagedData) {
         return res.status(404).json({ error: 'Data not found for the specified city' });
