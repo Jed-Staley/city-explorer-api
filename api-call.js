6dc0aadf029bc6d378688d51d4f713f6d8445532
Jed Staley <JNStaley1@gmail.com>
2024-04-05 18:23:56 -0400

    Added cache

--- api-call.js
+++ api-call.js
@@ -1,9 +1,19 @@
 const axios = require('axios');
+const cache = require('./cache');
 
-const callAPI = (app, receiveURL, requestURL, dataMassage) => {
+const callAPI = (app, receiveURL, cacheProp, requestURL, dataMassage) => {
   app.get(receiveURL, async (req, res) => {
+    const city = req.query.city;
+    if (cache[city]) {
+      const currentTime = new Date();
+      const expired = (currentTime.getTime() - cache[city].timeCached.getTime()) > 86400000;
+      const dayPassed = currentTime.getDate() !== cache[city].timeCached.getDate();
+      if (!expired && !dayPassed) {
+        return res.json(cache[city][cacheProp]);
+      }
+    }
+  
     try {
-      const city = req.query.city;
       console.log('contacting', requestURL(city));
       const response = await fetch(requestURL(city));
       const parsedData = await response.json();
@@ -12,6 +22,9 @@ const callAPI = (app, receiveURL, requestURL, dataMassage) => {
       if (!massagedData) {
         return res.status(404).json({ error: 'Data not found for the specified city' });
       }
+
+      cache[city].timeCached = new Date();
+      cache[city][cacheProp] = massagedData;
       res.json(massagedData);
 
     } catch (error) {
--- weather.js
+++ weather.js
@@ -5,7 +5,7 @@ const handleWeather = (app) => {
   callAPI(app, '/api/weather', (city) => `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${weatherAPIkey}&days=7`, (parsedData) => {
     let returnArr = []
     for (let i = 0; i < parsedData.data.length; i++) {
-      returnArr.push({ date: parsedData.data[i].datetime, description: parsedData.data[i].weather.description });
+      returnArr.push({ description: parsedData.data[i].weather.description });
     }
     return returnArr;
   })
