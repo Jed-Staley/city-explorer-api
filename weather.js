889149e7828ef9f5ab5f7e820a8c601d88eb624d
Jed Staley <JNStaley1@gmail.com>
2024-04-05 18:27:43 -0400

    fixed bug

--- weather.js
+++ weather.js
@@ -2,10 +2,10 @@ const callAPI = require('./api-call');
 const weatherAPIkey = process.env.WEATHER_API_KEY;
 
 const handleWeather = (app) => {
-  callAPI(app, '/api/weather', (city) => `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${weatherAPIkey}&days=7`, (parsedData) => {
+  callAPI(app, '/api/weather', 'weather', (city) => `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${weatherAPIkey}&days=7`, (parsedData) => {
     let returnArr = []
     for (let i = 0; i < parsedData.data.length; i++) {
-      returnArr.push({ description: parsedData.data[i].weather.description });
+      returnArr.push({ date: parsedData.data[i].datetime, description: parsedData.data[i].weather.description });
     }
     return returnArr;
   })
